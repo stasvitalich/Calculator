f88bbcd0014820267bcc6fbf1a3c494d9cc0b513
stasvitalich <stasvitalich1994@gmail.com>
2023-02-13 18:21:48 +0600

    Some prohibited calculation patterns were blocked. Minus functionality was added.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     var lastNumeric = false
     var lastDot = false
+    val symbols = Regex("[/*+]")
+    var doubleDot = false
 
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
@@ -24,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
 
     private fun buttonsClick() = with(binding) {
         button0.setOnClickListener {
@@ -82,17 +85,82 @@ class MainActivity : AppCompatActivity() {
                 textResult.append(".")
                 lastDot = true
                 lastNumeric = false
+
             }
         }
 
-        buttonDivide.setOnClickListener { }
-        buttonMultiply.setOnClickListener { }
-        buttonMinus.setOnClickListener { }
-        buttonPlus.setOnClickListener { }
-        buttonEqual.setOnClickListener { }
+        buttonDivide.setOnClickListener {
+            if (textResult.text.isNotEmpty() && (!symbols.containsMatchIn(textResult.text)) && (lastNumeric == true)) {
+                textResult.append("/")
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+
+        buttonMultiply.setOnClickListener {
+            if (textResult.text.isNotEmpty() && (!symbols.containsMatchIn(textResult.text)) && (lastNumeric == true)) {
+                textResult.append("*")
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+
+        buttonMinus.setOnClickListener {
+            if (textResult.text.isEmpty()) {
+                textResult.append("-")
+                lastNumeric = false
+                lastDot = false
+            } else if (textResult.text.isNotEmpty() && textResult.text.last() != '-') {
+                textResult.append("-")
+                lastNumeric = false
+                lastDot = false
+            }
+
+        }
+
+        buttonPlus.setOnClickListener {
+            if (textResult.text.isNotEmpty() && (!symbols.containsMatchIn(textResult.text)) && (lastNumeric == true)) {
+                textResult.append("+")
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+
+        buttonEqual.setOnClickListener {
+            if (lastNumeric) {
+                var tvValue = textResult.text.toString()
+                var prefix = ""
+                try {
+                    if (tvValue.startsWith("-")){
+                        prefix = "-"
+                        tvValue = tvValue.substring(1)
+                    }
+
+                    if (tvValue.contains("-")){
+                        val splitValue = tvValue.split("-")
+
+                        var one = splitValue[0]
+                        var two = splitValue[1]
+
+                        if (prefix.isNotEmpty()){
+                            one = prefix + one
+                        }
+
+                        textResult.text = (one.toDouble() - two.toDouble()).toString()
+                    }
+
+
+                } catch (e: ArithmeticException) {
+                    e.printStackTrace()
+                }
+            }
+        }
         buttonClear.setOnClickListener {
             textResult.text = ""
         }
     }
 
 }
+
+
+
